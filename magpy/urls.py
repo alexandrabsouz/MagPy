7fcaeb0639e075568c380ab279be3295de8319e0
alexandra <alexandrabsouz@gmail.com>
2021-08-29 20:22:47 -0300

    feat: configurando documentação swagger da api

--- magpy/urls.py
+++ magpy/urls.py
@@ -19,10 +19,36 @@ from rest_framework import routers
 
 from api import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MAGPY API",
+      default_version='v1',
+      description="Controle de versão de pacotes utilizando o PyPI",
+      terms_of_service="https://www.magpy.com/policies/terms/",
+      contact=openapi.Contact(email="contact@magpy.local"),
+      license=openapi.License(name="Instruct License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+
+   ...
+]
+
+
 router = routers.SimpleRouter()
 router.register(r'projects', views.ProjectViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
