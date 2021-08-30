20191aa826531b2607155410b4353c0ba1adec2d
alexandra <alexandrabsouz@gmail.com>
2021-08-30 08:22:22 -0300

    style: limpa alguns comentarios do código

--- api/serializers.py
+++ api/serializers.py
@@ -1,32 +1,32 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ParseError, ValidationError
+from rest_framework.exceptions import ParseError
 from .models import PackageRelease, Project
 import requests
 
 
+error = {"error": "One or more packages doesn't exist"}
 class PackageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PackageRelease
         fields = ['name', 'version']
         extra_kwargs = {'version': {'required': False}}
 
-    version = serializers.SerializerMethodField() # falo que o campo version e um metodo personalisado
+    version = serializers.SerializerMethodField() 
 
     def get_version(self, instance): #função para obter a versão
         r = requests.get(f"https://pypi.org/pypi/{instance.name}/json") # busca na api do pypi
 
         if r.status_code != 200: # verifica o status code
-            raise serializers.ValidationError(f"Package {instance.name} not found on PyPI") # caso nao encontre o pacote
+            raise serializers.ValidationError(f"Package {instance.name} not found on PyPI") 
 
-        data_json = r.json() # pega o json
-
+        data_json = r.json()
 
         if data_json['info']['version'] != instance.version: # verifica se a versão e diferente
-            instance.version = data_json['info']['version'] # atualiza a versão
+            instance.version = data_json['info']['version']
 
             instance.save() # salva no banco de dados
 
-        return instance.version # retorna a versão
+        return instance.version # retorna a versão 
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -38,26 +38,20 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         
-        # TODO
-        # - Processar os pacotes recebidos
-        # - Persistir informações no banco
         packages_data = validated_data['packages'] # lista de pacotes
         projeto = Project.objects.create(name=validated_data['name']) # cria o projeto
         
-        for package in packages_data:                           # percorro todos os pacoters
-            _package = package["name"]                                 # pego o nome do pacote
-            r = requests.get(f"https://pypi.org/pypi/{_package}/json") # busca na api do pypi
+        for package in packages_data:                           
+            package_name = package["name"]                                 
+            r = requests.get(f"https://pypi.org/pypi/{package_name}/json") # busca na api do pypi
 
 
-            if r.status_code != 200: # verifica o status code
-                projeto.delete() # caso nao encontre o pacote
-
-                raise ParseError("One or more packages doesn't exist") # caso nao encontre o pacote
+            if r.status_code != 200: 
+                projeto.delete() 
+                raise ParseError(error) # caso nao encontre o pacote
 
             PackageRelease.objects.create(project=projeto ,**package) # cria o pacote
 
         return projeto # retorna o projeto
         
-        
-        #packages = validated_data['packages']
-        #return Project(name=validated_data['name'])
+    
